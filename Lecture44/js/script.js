5670562c44fdbcb29871d375a8f27d2199239b5a
David Gengenbach <62947135+dgengenbach@users.noreply.github.com>
2020-04-09 10:09:47 -0600

    test

--- Lecture44/js/script.js
+++ Lecture44/js/script.js
@@ -1,4 +1,9 @@
 // Default values
+// var serverIP = resultInput.;
+
+
+var serverIP = document.getElementByID(resultInput);
+serverIP = serverIP || "Enter C360 Server IP";
 function orderChickenWith(sideDish) {
   sideDish = sideDish || "whatever!";
   console.log("Chicken with " + sideDish);
